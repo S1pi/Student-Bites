2348ab6202eaf5e8f8918f211920e86e7a52ebca
S1pi <miikavsipila@gmail.com>
2024-10-13 18:31:34 +0300

    Add: darkmode

--- darkmode.ts
+++ darkmode.ts
@@ -3,13 +3,8 @@ const darkmodeToggle = document.getElementById(
 ) as HTMLInputElement | null;
 const body = document.body as HTMLBodyElement;
 
-const sunBtn = document.getElementById("sun") as HTMLElement;
-const moonBtn = document.getElementById("moon") as HTMLElement;
-
 if (darkmodeToggle) {
   darkmodeToggle.addEventListener("change", () => {
-    body.classList.toggle("dark-mode");
-    // moonBtn.style.display = "none";
-    // sunBtn.style.display = "inline-block";
+    body.classList.toggle("dark-theme");
   });
 }
