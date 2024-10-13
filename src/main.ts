841bc85ccea5b1b143417283a796838211b8937a
S1pi <miikavsipila@gmail.com>
2024-10-13 19:26:34 +0300

    Created build for publish on metropolia server

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,13 @@ import { Restaurant } from "./interfaces/Restaurant";
 
 const apiUrl = "https://media1.edu.metropolia.fi/restaurant/api/v1";
 
-const restaurants: Restaurant[] = await fetchData(apiUrl + "/restaurants");
+const loadRestaurants = async (): Promise<void> => {
+  try {
+    const restaurants: Restaurant[] = await fetchData(apiUrl + "/restaurants");
+    console.log(restaurants);
+  } catch (err) {
+    throw new Error(`Error fetching restaurants: + ${err}`);
+  }
+};
 
-console.log(restaurants);
+loadRestaurants();
